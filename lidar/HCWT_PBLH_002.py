a1aec31ced9c93c5752a999e90e470e457c601a7
moroots <64803960+moroots@users.noreply.github.com>
2022-01-14 14:49:54 -0500

    clean and fast

--- lidar/HCWT_PBLH_002.py
+++ lidar/HCWT_PBLH_002.py
@@ -7,24 +7,24 @@ Created on Thu Jan 13 18:30:32 2022
 
 # %% Import Packages
 
+# ploting
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.colors import LogNorm
+
+# data processing
+from scipy.signal import savgol_filter
+from scipy.signal import find_peaks
+import xarray as xr
+import pandas as pd
+
+# utilities
+from pathlib import Path
+from tqdm import tqdm
+from numba import jit
 import numpy as np
 import time
-import matplotlib.dates as mdates
-import xarray as xr
-import os
-from glob import glob
-from matplotlib.colors import LogNorm
-import pandas as pd
-from scipy.signal import savgol_filter
-from scipy.signal import convolve
 
-from scipy.signal import find_peaks
-from pathlib import Path
-
-from numba import jit
-
-from metpy.units import units
 #%% Function Space
 
 def importing_ceilometer(FilePaths, **kwargs):
@@ -131,161 +131,164 @@ def moving_average(data, window):
     df_avg=df_avg.to_numpy()
     return df_avg.T
 
-#%% Importing the data
-
-FilePaths = [r"C:/Users/meroo/OneDrive - UMBC/Research/Data/Celiometer/test_data/20210518_Catonsville-MD_CHM160112_000.nc"]
-data, files = importing_ceilometer(FilePaths)
-keys = list(data.keys())
-
-#%% Testbed
-start_time = time.time()
-
-a = 5
-alt_stop = np.where(data[keys[0]]["range"]>=8000)[0][0]
-alt_start = np.where(data[keys[0]]["range"]>=100)[0][0]
-
-smoothed = savgol(data[keys[0]]["beta_raw"][alt_start:alt_stop, :])
-
-#%%
-
-a = [1, 10]
-a_vals = np.arange(a[0], a[1])
-# pblh = np.zeros((data["backscatter"][alt_start:alt_stop, :]).shape)
-c = np.zeros((len(data[keys[0]]["beta_raw"][alt_start:alt_stop]), len(a_vals)))
-alts = data[keys[0]]["range"][alt_start:alt_stop]
-c_avg = np.zeros((data[keys[0]]["beta_raw"][alt_start:alt_stop, :]).shape)
-
-for i in range(data[keys[0]]["beta_raw"][alt_start:alt_stop, :].shape[1]):
-    for a, j in zip(a_vals, range(0, len(c)+1)):
-        c[:,j]= covariance_profile(data[keys[0]]["beta_raw"][alt_start:alt_stop, i], a, alts)
-        print(f"index: {i}, a = {a}")
-    c_avg[:, i] = np.mean(c, axis=1)
-
-end_time = time.time()
-print(f"Execuded in {round(end_time - start_time, 3)} seconds")
-
-#%% Finding Peaks
-
-# test_spot = 5000
-# alt = data["alt"][alt_start:alt_stop]
-# x = c_avg[:, test_spot]
-
-# peaks_top, properties = find_peaks(x, height=0, threshold=500, distance=50, prominence=(5e3, 5e6))
-# peaks_bottom, properties = find_peaks(-1*x, height=0, threshold=100, distance=50, prominence=(5e3, 5e6))
-
-# plt.figure()
-# plt.plot(x, alt)
-# plt.plot(x[peaks_top], alt[peaks_top], '^', color="tab:red")
-# plt.plot(x[peaks_bottom], alt[peaks_bottom], 'v', color="tab:red")
-# plt.xlim(-1e6, 1e7)
-# plt.ylabel("Altitude")
-# plt.title(f"20210517_Catonsville-MD_CHM160112_000.nc: t={test_spot}")
-# plt.show()
-
-# t_top = np.full(shape=alt[peaks_top].shape, fill_value=data["time"][test_spot])
-# t_bottom = np.full(shape=alt[peaks_bottom].shape, fill_value=data["time"][test_spot])
-
-# plt.figure(figsize=(12, 6), constrained_layout=True)
-# im = plt.pcolormesh(data["time"],data["alt"],data["backscatter"],cmap='jet', norm=LogNorm())
-# plt.axvline(data["time"][test_spot+30], linestyle='--', color="k")
-# plt.axvline(data["time"][test_spot-30], linestyle='--', color="k")
-# plt.plot(t_top, alt[peaks_top], 'v', color="pink")
-# plt.plot(t_bottom, alt[peaks_bottom], '^', color="pink")
-# plt.ylim([0,6000])
-# cbar = plt.colorbar()
-# im.set_clim(vmin=10**3.5, vmax=10**8.5)
-# cbar.set_label('Aerosol Backscatter')
-# plt.xlabel('Datetime (UTC)')
-# plt.ylabel('Altitude (m AGL)')
-# plt.title("20210517_Catonsville-MD_CHM160112_000.nc")
-# plt.show()
-
-#%% PLotting Pblh
-
-peaks_bottom = np.zeros((len(c_avg[0,:]), 50))
-for i in range(0, len(c_avg[0,:])):
-    peaks, properties = find_peaks(-1*c_avg[:, i], height=0, threshold=500, distance=50, prominence=(5e3, 5e4))
-    order_test = sorted(((value, index) for index, value in enumerate(properties["prominences"])), reverse=True)
-    # print(peaks)
-    for j in range(0, len(peaks)):
-        peaks_bottom[i, j] = peaks[order_test[j][1]]
-        # print(peaks[j])
-    # print(i)
-peaks_bottom = peaks_bottom.astype(int)
-
-#%%
-
-df = pd.read_csv(r"C:/Users/meroo/OneDrive - UMBC/Research/Code/Python/FromKylie/New folder/vanessa_lufft_pbl.csv", header=None)
-
-#%%
-
-# plt.figure(figsize=(12, 6), constrained_layout=True)
-# # im = plt.pcolormesh(data["time"],data["alt"],data["backscatter"],cmap='jet', norm=LogNorm())
-# # im = plt.pcolormesh(data["time"],data["alt"],df_avg.T,cmap='jet', norm=LogNorm())
-# # im = plt.pcolormesh(data["time"],data["alt"][alt_start:alt_stop],smoothed,cmap='jet', norm=LogNorm())
-# # im = plt.pcolormesh(data["time"],data["alt"],test,cmap='jet', norm=LogNorm())
-# # plt.plot(data["time"], alt[peaks_bottom[:, 2]], '^', color="pink")
-# # plt.plot(data["time"], df[1], 'x', color="k")
-# plt.ylim([0,6000])
-# cbar = plt.colorbar()
-# im.set_clim(vmin=10**3.5, vmax=10**8.5)
-# cbar.set_label('Aerosol Backscatter')
-# plt.xlabel('Datetime (UTC)')
-# plt.ylabel('Altitude (m AGL)')
-# plt.title("20210517_Catonsville-MD_CHM160112_000.nc")
-# plt.show()
-
-#%% TESTBED
-# Peak Picking Function
-
-# @jit(nopython=True)
-def pick_peaks(data,peak_data,PBL_index0, units=1):
-    len_peaks = peak_data.shape[0]
-    selected_peaks = np.zeros(len_peaks)
-    selected_peaks[:] = -888
-    tt = data["time"]
-    zz = data["range"]*units
+def pick_peaks_001(data,peak_data,apriori_height=2000, Dy=200, Dt=np.timedelta64(30,'m'), **kwargs):
     
+    len_peaks = np.int64(len(peak_data))
+    selected_peaks = np.ones(len_peaks)*-1
+    blanks = 0
     for i in np.arange(len_peaks):
-    # for i in np.arange(100):
         if i==0:
-            selected_peaks[i] = PBL_index0
-            i_m1 = i
+            selected_peaks[i] = np.where(data["range"] >= apriori_height)[0][0]
+            j = i
         else:
-            dt = tt[i]-tt[i_m1]
-            if dt<=np.timedelta64(30,'m'):
-                # test peak 1
-                if (zz[peak_data[i,0]]-zz[selected_peaks[i_m1].astype(int)])<(200*units) and peak_data[i,0]!=0:
-                    selected_peaks[i] = peak_data[i,0].astype(int)
-                    i_m1 = i
+            dt = data["time"][i]-data["time"][j]
+            if dt<=Dt:
+                if (np.abs(data["range"][peak_data[i,0]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,0]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,0])
+                    j = i
                     continue
-                elif (zz[peak_data[i,1]]-zz[selected_peaks[i_m1].astype(int)])<(200*units) and peak_data[i,1]!=0:
-                    selected_peaks[i] = peak_data[i,1].astype(int)
-                    i_m1 = i
+                elif (np.abs(data["range"][peak_data[i,1]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,1]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,1])
+                    j = i
                     continue
-                elif (zz[peak_data[i,2]]-zz[selected_peaks[i_m1].astype(int)])<(200*units) and peak_data[i,2]!=0:
-                    selected_peaks[i] = peak_data[i,2].astype(int)
-                    i_m1 = i
+                elif (np.abs(data["range"][peak_data[i,2]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,2]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,2])
+                    j = i
                     continue
-                elif (zz[peak_data[i,3]]-zz[selected_peaks[i_m1].astype(int)])<(200*units) and peak_data[i,3]!=0:
-                    selected_peaks[i] = peak_data[i,3].astype(int)
-                    i_m1 = i
+                elif (np.abs(data["range"][peak_data[i,3]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,3]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,3])
+                    j = i
                     continue
                 else:
                     selected_peaks[i] = np.int64(-999)
                     continue
-            if dt>np.timedelta64(10,'m'):
-                print('No viable continuous peaks found within 10 mins.',\
-                      'Peak identification stopped at time:',tt[i].astype(str)[11:19],\
-                      ',index:',i)
-                break
+            else:
+                blanks += 1
+                if blanks < 0.1*len_peaks:
+                    print("Too few points, attempt new apriori_height")
+                    break
                 
-    return selected_peaks.astype(int)
+    return np.int64(selected_peaks)
 
-
-for key in keys:
-    data[key]["selected_peaks"] = pick_peaks(data[key],peaks_bottom,132)
+def pick_peaks_002(data,peak_data, Dy=200, Dt=np.timedelta64(30,'m'), **kwargs):
     
-    fig, ax = plt.subplots()
-    ax.pcolormesh(data[key]["time"], data[key]["range"], data[key]["beta_raw"])
-    # ax.plot(data[key]["selected_peaks"])
+    len_peaks = np.int64(len(peak_data))
+    selected_peaks = np.ones(len_peaks)*-1
+    blanks = 0
+    
+    for i in np.arange(len_peaks):
+        if i==0:
+            selected_peaks[i] = peak_data[0,0]
+            j = i
+        else:
+            dt = data["time"][i]-data["time"][j]
+            if dt<=Dt:
+                if (np.abs(data["range"][peak_data[i,0]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,0]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,0])
+                    j = i
+                    continue
+                elif (np.abs(data["range"][peak_data[i,1]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,1]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,1])
+                    j = i
+                    continue
+                elif (np.abs(data["range"][peak_data[i,2]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,2]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,2])
+                    j = i
+                    continue
+                elif (np.abs(data["range"][peak_data[i,3]]-data["range"][np.int64(selected_peaks[j])])<=(Dy)) and (peak_data[i,3]!=0):
+                    selected_peaks[i] = np.int64(peak_data[i,3])
+                    j = i
+                    continue
+                else:
+                    selected_peaks[i] = np.int64(-999)
+                    continue
+            else:
+                blanks += 1
+                if blanks < 0.1*len_peaks:
+                    print("Too few points, attempt new apriori_height")
+                    break
+                
+    return np.int64(selected_peaks)
+
+def estimate_pblh(data, a_vals, alt_constraint=[100, 4000], **kwargs):
+    
+    if not "parms__peak_selection" in kwargs.keys():
+        parms__peak_selection = {"apriori_height":1500, "Dy":500, "Dt":np.timedelta64(60,'m')}
+            
+    for key in keys:
+        
+        # Smoothing
+        alt_stop = np.where(data[key]["range"]>=alt_constraint[1])[0][0]
+        alt_start = np.where(data[key]["range"]>=alt_constraint[0])[0][0]
+        smoothed = savgol(data[key]["beta_raw"][alt_start:alt_stop, :])
+        
+        # Covariance Transform
+        c = np.zeros((len(smoothed), len(a_vals)))
+        alts = data[key]["range"][alt_start:alt_stop]
+        c_avg = np.zeros(smoothed.shape)
+        
+        for i in np.arange(smoothed[:, :].shape[1]):
+            for a, j in zip(a_vals, np.arange(0, len(c)+1)):
+                c[:,j]= covariance_profile(smoothed[:, i], a, alts)
+            c_avg[:, i] = np.mean(c, axis=1)
+    
+        # Finding Peaks
+        peaks_bottom = np.zeros((len(c_avg[0,:]), 50))
+        for i in np.arange(0, len(c_avg[0,:])):
+            peaks, properties = find_peaks(-1*c_avg[:, i], height=0, prominence=(5e3, 5e4))
+            order_test = sorted(((value, index) for index, value in enumerate(properties["prominences"])), reverse=True)
+            for j in range(0, len(peaks)):
+                peaks_bottom[i, j] = peaks[order_test[j][1]]
+        peaks_data = np.int64(peaks_bottom)
+        
+        # Selecting PBLH
+        selected_peaks = pick_peaks_002(data[key],peaks_data, **parms__peak_selection)
+        df = pd.DataFrame({"peaks": selected_peaks, "time": data[key]["time"]})
+        df = df[df.peaks > 0]
+        df["heights"] = data[key]["range"][df["peaks"]]
+        
+        data[key]["pblh"] = df   
+        data[key]["peaks"] = peaks_data
+        data[key]["c_avg"] = c_avg
+        data[key]["smoothed"] = smoothed
+        data[key]["selected_peaks"] = selected_peaks
+        data[key]["alt_start_stop"] = [alt_start, alt_stop]
+    
+    return data
+
+
+if __name__ == "__main__":
+    
+    file_start_time = time.time()
+    
+    FilePaths = [r"C:/Users/meroo/OneDrive - UMBC/Research/Data/Celiometer/test_data/20210518_Catonsville-MD_CHM160112_000.nc"]
+    data, files = importing_ceilometer(FilePaths)
+    keys = list(data.keys())
+
+    data = estimate_pblh(data=data, a_vals=np.arange(1, 500, 10))
+
+#%%
+    for key in keys:
+        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(15,8))
+        
+        im1 = ax1.pcolormesh(data[key]["time"], data[key]["range"], data[key]["beta_raw"], cmap='jet', norm=LogNorm())
+        im1.set_clim(vmin=10**4.5, vmax=10**5.2)
+        
+        alt_start, alt_stop = data[key]["alt_start_stop"]
+        
+        im2 = ax2.pcolormesh(data[key]["time"], data[key]["range"][alt_start:alt_stop], data[key]["c_avg"], cmap='jet', norm=LogNorm())
+        im2.set_clim(vmin=10**-3, vmax=10**2)
+        
+        ax1.plot(data[key]["pblh"].time,data[key]["pblh"].heights, "^", color="k",markersize=3)
+    ax1.set_ylim([200,4000])
+    ax2.set_ylim([200,4000])
+    cbar = fig.colorbar(im1, ax=ax1, pad=0.01)
+    cbar = fig.colorbar(im2, ax=ax2, pad=0.01)
+    cbar.set_label('Aerosol Backscatter')
+    ax2.set_xlabel('Datetime (UTC)')
+    ax1.set_ylabel('Altitude (m AGL)')
+    plt.suptitle("20210517_Catonsville-MD_CHM160112_000.nc")
+    plt.show()
+
+    file_end_time = time.time()
+    print(f"Execuded file in {round(file_end_time - file_start_time, 3)} seconds")
